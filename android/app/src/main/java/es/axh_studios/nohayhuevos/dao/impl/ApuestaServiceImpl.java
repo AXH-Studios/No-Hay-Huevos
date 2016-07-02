258a3bd6725f2a21f53d50b34d23a9a71e32bc3d
Alejandro Larios <alesevilla92@gmail.com>
2016-07-02 18:27:13 +0200

    ñññ

---
+++ android/app/src/main/java/es/axh_studios/nohayhuevos/dao/impl/ApuestaServiceImpl.java
@@ -0,0 +1,44 @@
+package es.axh_studios.nohayhuevos.dao.impl;
+
+import java.io.IOException;
+
+import es.axh_studios.nohayhuevos.utils.ApiUtils;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+/**
+ * Created by Alejandro on 02/07/2016.
+ */
+public class ApuestaServiceImpl {
+
+    private OkHttpClient client = new OkHttpClient();
+    private static final MediaType JSON = ApiUtils.JSON;
+    private static final String URL_BASE = ApiUtils.URL_BASE;
+
+    public String listPikes(String user) throws IOException {
+        String url = URL_BASE+"/api/user/"+ user +"/porra/";
+
+        Request request = new Request.Builder()
+                .url(url)
+                .get()
+                .build();
+        Response response = client.newCall(request).execute();
+        return response.body().string();
+    }
+
+    public String createPike(String json) throws IOException {
+        String url = URL_BASE+"api/payment";
+
+        RequestBody body = RequestBody.create(JSON, json);
+        Request request = new Request.Builder()
+                .url(url)
+                .put(body)
+                .build();
+        Response response = client.newCall(request).execute();
+        return response.body().string();
+    }
+
+}
